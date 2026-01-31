5281478610af840664bac1085b665181e194f6ba
sinomnomnom <152234476+sinomnomnom@users.noreply.github.com>
2026-01-31 01:05:02 -0500

    Initial commit

---
+++ Assets/BillboardScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour
+{
+    Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = mainCamera.transform.rotation;
+    }
+}
